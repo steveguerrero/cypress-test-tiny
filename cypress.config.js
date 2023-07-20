736170d680e4c14c6870059ba45d8ca98710be54
Steve <43235249+steveguerrero@users.noreply.github.com>
2023-07-20 09:55:05 +0200

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,25 @@
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:4272',
+    retries: {
+      runMode: 3,
+      openMode: 0,
+    },
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: false,
+    experimentalInteractiveRunEvents: true,
+    async setupNodeEvents(on, config) {
+        on('after:run', (results) => {
+          console.log("Cypress Test after:run");
+        });
+        
+        on('after:spec', (results) => {
+          console.log("Cypress Test after:spec");
+        });
+      }
+      return config;
+    },
+  },
   },
 }
